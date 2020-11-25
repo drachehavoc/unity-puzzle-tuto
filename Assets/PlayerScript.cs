54ec4abfead6a1c7b8642fc8a3cb36770fb46049
Varela <>
2020-11-25 03:09:12 -0300

    tuto-multiplayer-local

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    int playerNo;
+
+    void Start()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+
+        playerNo = playerInput.playerIndex;
+
+        print($"Player {playerNo} joined the game.");
+
+        playerInput.actions["Jump"].performed += FnJump;
+    }
+
+    void FnJump(InputAction.CallbackContext context)
+    {
+        print($"Player {playerNo} JUMPED");
+    }
+}
